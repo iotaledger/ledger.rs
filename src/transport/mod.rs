286ef4f027f5d8e7efe74e013772477103c4f86a
shufps <shufps80@gmail.com>
2022-10-06 18:04:15 +0200

    added hidwrapper from old ledger-rs lib

--- src/transport/mod.rs
+++ src/transport/mod.rs
@@ -1,11 +1,43 @@
 pub mod errors;
 pub mod transport_tcp;
 
-use hidapi::HidApi;
-
 use crate::transport::transport_tcp::{Callback, TransportTCP};
 use crate::APIError;
 use ledger_transport_hid::TransportNativeHID;
+
+use lazy_static::lazy_static;
+use std::cell::RefCell;
+use std::sync::{Arc, Mutex, Weak};
+
+struct HidApiWrapper {
+    _api: RefCell<Weak<Mutex<hidapi::HidApi>>>,
+}
+
+lazy_static! {
+    static ref HIDAPIWRAPPER: Arc<Mutex<HidApiWrapper>> =
+        Arc::new(Mutex::new(HidApiWrapper::new()));
+}
+
+impl HidApiWrapper {
+    fn new() -> Self {
+        HidApiWrapper {
+            _api: RefCell::new(Weak::new()),
+        }
+    }
+
+    fn get(&self) -> Result<Arc<Mutex<hidapi::HidApi>>, APIError> {
+        let tmp = self._api.borrow().upgrade();
+
+        if let Some(api_mutex) = tmp {
+            return Ok(api_mutex);
+        }
+
+        let hidapi = hidapi::HidApi::new().map_err(|_| APIError::TransportError)?;
+        let tmp = Arc::new(Mutex::new(hidapi));
+        self._api.replace(Arc::downgrade(&tmp));
+        Ok(tmp)
+    }
+}
 
 #[derive(Copy, Clone)]
 #[allow(clippy::upper_case_acronyms)]
@@ -45,7 +77,10 @@ pub fn create_transport(
     let transport = match transport_type {
         TransportTypes::TCP => Transport::TCP(TransportTCP::new("127.0.0.1", 9999, callback)),
         TransportTypes::NativeHID => {
-            let api = HidApi::new().map_err(|_| APIError::TransportError)?;
+            let apiwrapper = HIDAPIWRAPPER.lock().map_err(|_| APIError::TransportError)?;
+            let api_mutex = apiwrapper.get()?;
+            let api = api_mutex.lock().map_err(|_| APIError::TransportError)?;
+
             Transport::NativeHID(
                 TransportNativeHID::new(&api).map_err(|_| APIError::TransportError)?,
             )
